a8365e819997766ff08b2115c9c597051a6c999f
dsar-svg <dariomedina2619@gmail.com>
2026-05-13 10:44:32 -0400

    Add GeneralStorePerformancePage component

---
+++ app/rendimiento-tiendas/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { StorePerformanceView } from "@/components/store-performance/StorePerformanceView"
+
+export default function GeneralStorePerformancePage() {
+  return (
+    <div className="p-4 lg:p-6">
+      <StorePerformanceView
+        sheetRange="Sheet1!A2:ZZ"
+        title="Rendimiento de Tiendas (General)"
+      />
+    </div>
+  )
+}
